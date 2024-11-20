5ea479cf8f5305a0476bcde7ae21ef5dafa20e18
Jarvis Choy <choyja2@oocl.com>
2024-11-20 20:46:06 +0800

    refactor: introduce NEW_LINE constant for new line character

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -3,13 +3,14 @@ import java.util.stream.Collectors;
 
 public class WordFrequencyGame {
     public static final String SPACE_REGEX = "\\s+";
+    public static final String NEW_LINE = "\n";
 
     public String getWordFrequency(String sentence) {
         Map<String, Long> wordCountMap = countWordFrequencies(sentence);
         return wordCountMap.entrySet().stream()
                 .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
                 .map(entry -> new WordFrequency(entry.getKey(), entry.getValue().intValue()).toString())
-                .collect(Collectors.joining("\n"));
+                .collect(Collectors.joining(NEW_LINE));
     }
 
     public Map<String, Long> countWordFrequencies(String sentence) {
